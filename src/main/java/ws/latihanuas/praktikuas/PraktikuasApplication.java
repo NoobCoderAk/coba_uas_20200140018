ce9c57ad0dd1b95b7c959cc87079fca74045a948
NoobCoderAk <abidakbar0001@gmail.com>
2022-12-30 15:51:31 +0700

    project latihan uas

---
+++ src/main/java/ws/latihanuas/praktikuas/PraktikuasApplication.java
@@ -0,0 +1,13 @@
+package ws.latihanuas.praktikuas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraktikuasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraktikuasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ws/latihanuas/praktikuas/PraktikuasApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.latihanuas.praktikuas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraktikuasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
